7f95d1c200718c79f0cbcf7dd164b7b59573dcc4
t11915jr <jeremy.roe@student.manchester.ac.uk>
2021-11-07 17:04:59 +0000

    Implementing bot

--- src/bot.js
+++ src/bot.js
@@ -34,6 +34,12 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 });
                 break;
             // Just add any case commands if you want to..
+            case 'intro':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+                break;
         }
     }
 });
