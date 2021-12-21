8384de0513687d145bc08b23fa81bf24c8532adb
t11915jr <jeremy.roe@student.manchester.ac.uk>
2021-12-21 03:32:08 +0000

    Created basic commands that the bot responds to (`ping`, `server`, `hello`)

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # TTRPGSoc_Discord_Bot
 Repository for UoM Tabletop Roleplaying Game Society Discord index
+
+## Installation
+
+- `git clone https://github.com/jeremyr03/TTRPGSoc_Discord_Bot`
+- `npm install`
+- `npm run build`
+- `set BOT_TOKEN=<your bot token>`
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
-const { token } = require('./auth.json');
+const { token, prefix } = require('../auth.json');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -10,8 +10,17 @@ client.once('ready', () => {
     console.log('Ready!');
 });
 
+client.on('message', message => {
+    console.log(message.content);
+    if (message.content === `${prefix}ping`) {
+        message.channel.send('Pong');
+    }else if (message.content === `${prefix}server`){
+        message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+    }else if (message.content === `${prefix}hello`){
+        message.channel.send(`Hello ${message.author.username}! I am a bot for the UoM TTRPG Soc.`);
+    }
+});
+
 
 // Login to Discord with your client's token
 client.login(token);
-
-
